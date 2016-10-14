8f6144867c1a0d3b180704a0ff6d930404623a70
Julien Colomb <julien.colomb@casavaticano.com.br>
2016-10-14 18:56:17 -0300

    update

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Operation;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
@@ -15,6 +18,76 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/import", name="import operations")
+     */
+    public function importAction(Request $request)
+    {
+        if ($request->getMethod() == "POST") {
+
+            /**
+             * @var $file UploadedFile
+             */
+            $file = $request->files->get('operations');
+
+            $path = $file->getRealPath();
+
+            $csv = new \parseCSV($path);
+
+            $em = $this->getDoctrine()->getEntityManager();
+
+            $lastCreated = null;
+
+            $rows = $csv->data;
+
+            foreach ($rows as $row) {
+
+                if ( ! $row['description']) {
+                    continue;
+                }
+
+                $op = new Operation();
+                $op->setAmount($row['amount']);
+
+                if ($row['created']) {
+                    $created = $row['created'];
+
+                    $created = \DateTime::createFromFormat('d/m/Y', $created);
+                } else if ($lastCreated) {
+                    $created = $lastCreated;
+                } else {
+                    throw new \Exception('missing date');
+                }
+
+                $op->setOpDate($created);
+                $op->setCurrency($row['currency']);
+                $op->setDescription($row['description']);
+                $op->setTags($row['tags']);
+
+                $em->persist($op);
+
+                $lastCreated = $op->getOpDate();
+            }
+
+            $em->flush();
+
+
+            exit;
+
+
+
+            exit;
+
+        }
+
+
+
+        // replace this example code with whatever you need
+        return $this->render('default/import.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
---
+++ src/AppBundle/Entity/Operation.php
@@ -0,0 +1,173 @@
+<?php
+// src/AppBundle/Entity/Product.php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="operation")
+ */
+class Operation {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     **/
+    protected $opDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $tags;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    private $amount;
+
+    /**
+     * @ORM\Column(type="string", length=4)
+     */
+    private $currency;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Operation
+     */
+    public function setOpDate($created)
+    {
+        $this->opDate = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getOpDate()
+    {
+        return $this->opDate;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Operation
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set tags
+     *
+     * @param string $tags
+     *
+     * @return Operation
+     */
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+    /**
+     * Get tags
+     *
+     * @return string
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param string $amount
+     *
+     * @return Operation
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return string
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set currency
+     *
+     * @param string $currency
+     *
+     * @return Operation
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * Get currency
+     *
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+}
